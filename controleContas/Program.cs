4883ae3f5e2fbf089ccca4df80a88266fd3a1fb4
2022101877 <2022101877@LAB2-008>
2023-09-05 20:50:37 -0300

    Adicionar arquivos de projeto.

---
+++ controleContas/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using controleContas;
+
+Conta conta1 = new Conta(123456);
+Conta conta2 = new Conta(654321);
+conta2.Saldo = 2341.42m;
+conta1.Saldo = conta2.Saldo * 0.7m;
+
+//conta1._numero = 123456;
+Console.WriteLine($"Número da conta: {conta1.Numero}");
